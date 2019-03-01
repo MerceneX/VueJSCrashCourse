e1e44b27907719139ffab8805642c0f3fd44ccaa
Marko Gluhak <marko.gluhak@student.um.si>
2019-03-01 09:28:38 +0100

    Continued with the tutorial

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <ToDoItem/>
+        </div>
+    </div>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem.vue";
+
+export default {
+    name:"Todos",
+    components: {
+        ToDoItem
+        },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
